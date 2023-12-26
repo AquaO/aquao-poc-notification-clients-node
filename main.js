fc14670c501d7f13cf7bac6026c4f9dd164f9d35
Frédéric Lecointre <frederic.lecointre@aquao.fr>
2023-12-26 13:52:04 +0100

    Utilisation d'Express
    Permet de calculer la signature à partir du raw boy reçu par le serveur

--- main.js
+++ main.js
@@ -28,74 +28,68 @@ const __secret = Buffer.from(process.env.NOTIFICATION_SECRET, 'base64');
 /**
  * Rest server
  */
-const restServer = require('node-rest-server')
+const express = require('express');
+const bodyParser = require('body-parser');
+const restServer = express({type: "application/json"});
 
-// Server config
-const serverConfig = {
-    basePath: process.env.SERVER_ENDPOINT_BASE,
-    port: parseInt(process.env.SERVER_PORT),
-    delay: 1,
-    logger: {
-        enable: true,
-        debug: false,
-    },
-    cors: {
-        origin: "*",
-    },
-};
+// Debug
+restServer.use(function (request, response, next) {
+    console.debug(`Incoming request :  ${request.path}`);
+    next();
+});
 
-// Route config
-const routeConfig = {
-	'/client/created': {
-		method: 'POST',
-		status: 203,
-		header: {},
-		controller: (request) => onNotifyClient(request, 'CREATED'),
-	},
-	'/client/updated': {
-		method: 'POST',
-		status: 203,
-		header: {},
-		controller: (request) => onNotifyClient(request, 'UPDATED'),
-	},
-	'/client/deleted': {
-		method: 'POST',
-		status: 203,
-		header: {},
-		controller: (request) => onNotifyClient(request, 'DELETED'),
-	},
-	'/client/anonymized': {
-		method: 'POST',
-		status: 203,
-		header: {},
-		controller: (request) => onNotifyClient(request, 'ANONYMIZED'),
-	}
-};
+// Intercept the raw body
+restServer.use(bodyParser.json({
+    verify: (request, response, buffer) => {
+        request.rawBody = buffer;
+    }
+}));
+
+// Routes
+restServer.post(process.env.SERVER_ENDPOINT_BASE + '/client/created', (request, response) => {
+    onClientNotification(request, response, 'CREATED');
+});
+
+restServer.post(process.env.SERVER_ENDPOINT_BASE + '/client/updated', (request, response) => {
+    onClientNotification(request, response, 'UPDATED');
+});
+
+restServer.post(process.env.SERVER_ENDPOINT_BASE + '/client/deleted', (request, response) => {
+    onClientNotification(request, response, 'DELETED');
+});
+
+restServer.post(process.env.SERVER_ENDPOINT_BASE + '/client/anonymized', (request, response) => {
+    onClientNotification(request, response, 'ANONYMIZED');
+});
 
 /**
  * Run the whole thing
  */
-restServer(routeConfig, serverConfig);
+restServer.listen(process.env.SERVER_PORT, () => {
+    console.log("Server running on port " + process.env.SERVER_PORT);
+});
 
 // ------------- CONTROLLERS -------------------------------------------
 /**
  * 
  * @param {ExpressRequest} request 
+ * @param {ExpressResponse} request 
  * @param {string} status 
  */
-function onNotifyClient(request, status){
+function onClientNotification(request, response, status){
     
     let uid = request.headers['x-aquao-notification-uid'];
     let signature = request.headers['x-aquao-notification-sign'];
 
     try{
-        checkSignature(uid, signature);
+        checkSignature(request.rawBody, signature);
         let client = request.body;
         console.log(`SUCCESS ${status} notification for client '${client.code}'`);
     } catch(e){
         console.log(`FAILED ${status} notification : ${e.message}`);
     }
 
+    response.status(204).send();
 }
 
 
@@ -103,17 +97,17 @@ function onNotifyClient(request, status){
 
 /**
  * 
- * @param {string} uid 
+ * @param {Buffer} data 
  * @param {string} signature 
  * @returns 
  */
-function checkSignature(uid, signature){
+function checkSignature(data, signature){
 
-    if(uid == undefined || signature == undefined){
-        throw new Error(`Bad headers uid (${uid}) or signature (${signature})`);
+    if(signature == undefined){
+        throw new Error(`Bad headers signature (${signature})`);
     }
 
-    let computed = crypto.createHmac('sha256', __secret).update(uid).digest("base64");
+    let computed = crypto.createHmac('sha256', __secret).update(data).digest("base64");
 
     if(computed != signature){
         throw new Error(`Bad signature '${signature}' from computed '${computed}'`);
